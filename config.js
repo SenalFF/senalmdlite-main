2eb09014b2458d2259ec885f4c7dcd145bd1f9d7
SenalFF <mrsenalff@gmail.com>
2026-02-26 19:11:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "5FICATKC#CjGa3dwcRGNX0Mt0dsjznRMvGnASM-t2JYIFR-Ijwmg",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/SenalFF/senalmd/refs/heads/main/system/IMG-20251229-WA0001.jpg?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "═══〔 🤖 SENAL MD 〕═══
+    SESSION_ID: process.env.SESSION_ID || "5FICATKC#CjGa3dwcRGNX0Mt0dsjznRMvGnASM-t2JYIFR-Ijwmg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/SenalFF/senalmd/refs/heads/main/system/IMG-20251229-WA0001.jpg?raw=true",
+    ALIVE_MSG: process.env.ALIVE_MSG || `═══〔 🤖 SENAL MD 〕═══
 
 👋 Hello User!
 🧑‍💻 Developed By : *Mr Senal*
@@ -16,9 +17,6 @@ ALIVE_MSG: process.env.ALIVE_MSG || "═══〔 🤖 SENAL MD 〕═══
 🌐 Network          : Stable
 🚀 Ready for Commands!
 
-═══════════════════════",
-BOT_OWNER: '94774915917',  // Replace with the owner's phone number
-
-
-
+═══════════════════════`,  
+    BOT_OWNER: '94769872326',
 };
