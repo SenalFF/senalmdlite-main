0a1726c0c83e26e07aac787c2a98a335b842e725
SenalFF <mrsenalff@gmail.com>
2026-03-02 03:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ async function connectToWA() {
   const ishan = makeWASocket({
     logger: P({ level: 'silent' }),
     printQRInTerminal: false,
-    browser: Browsers.macOS("Firefox"),
+    browser: ['Mac OS', 'Firefox', '14.4.1'],
     auth: state,
     version,
     syncFullHistory: true,
