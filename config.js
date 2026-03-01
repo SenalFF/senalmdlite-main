07d6412c1d85e74dff3657683aad1c02bcc2cf87
SenalFF <mrsenalff@gmail.com>
2026-03-01 11:25:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "e0RTDQpZ#c2M-PlaWmP3QLdbIgXRweXhM4p08fWdp-XwGSYtBSuo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/SenalFF/senalmd/refs/heads/main/system/IMG-20251229-WA0001.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || `═══〔 🤖 SENAL MD 〕═══
 
