9f46a0741182661b18108da9a61dc3068f98a697
SenalFF <mrsenalff@gmail.com>
2026-03-06 06:06:53 +0530

    Update cinesubzdts.js

--- plugins/cinesubzdts.js
+++ plugins/cinesubzdts.js
@@ -26,7 +26,7 @@ cmd(
       await test.sendMessage(from, { react: { text: "⏳", key: mek.key } });
 
       const res = await axios.get(
-        `https://cinesubz-xi.vercel.app/api/details?url=${encodeURIComponent(q)}`
+        `https://cinesubz-production.up.railway.app/api/details?url=${encodeURIComponent(q)}`
       );
       const d = res.data;
 
